9c5f36cbb5ad9e36a76371ee3bc75779a92dd01e
user1 <user1@DESKTOP-KJ3O3NO>
2024-02-05 09:56:28 +0900

    삼항 연산자

--- Homework/src/IfTest.java
+++ Homework/src/IfTest.java
@@ -12,7 +12,33 @@ public class IfTest {
 		
 		//삼항 연산자 적용
 		grade = score >= 60 ? "통과": "재평가";
-		
 		System.out.println(grade);
+		
+	
+		//중첩 삼항 연산자
+		score = 90;
+		grade = score >= 90 ? "우수": (grade = score >= 60 ? "통과": "재평가");
+		System.out.println(grade);
+		
+		int a = 10;
+		int b = 20;
+		int result1 = a>b ? a++: b--;
+		
+		//삼항 연산자 -> if else문
+		int result2 = a<b ? a++: (b==0? a--: b++);
+		
+		System.out.println(result1 + "\t" + result2);
+		
+		a = 10;
+		b = 20;
+		int result3 = 0;
+		if(a<b) {
+			result3 = a++;
+		}else if(b == 0) {
+			result3 = a--;
+		}else {
+			result3 = b++;
+		}
+		System.out.println(result3);
 	}
 }

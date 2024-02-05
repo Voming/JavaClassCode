1bc504d8bbfb0fb1923fc924d90065338d5863a0
user1 <user1@DESKTOP-KJ3O3NO>
2024-02-05 16:04:06 +0900

    getter, setter

--- Homework/src/inclass/kh/Student.java
+++ Homework/src/inclass/kh/Student.java
@@ -2,8 +2,52 @@ package inclass.kh;
 
 //클래스 실습
 public class Student {
-	String id;
-	String name;
-	String phone;
-	int age;
+	// private 클래스 내부에서만 사용
+	private String id;
+	private String name;
+	private String phone;
+	private int age;
+	//접근제한자를 적지 않았다면 default
+	
+	// Getter / Setter 게터, 세터
+	//오른쪽 마우스 -> Source -> Generate getter...
+	void exam(String name) {
+		System.out.println( name + "님 시험을 진행합니다.");
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	void enterance() {
+		System.out.println("입실했습니다.");
+	}
 }
---
+++ Homework/src/inclass/kh/StudentRun.java
@@ -0,0 +1,39 @@
+package inclass.kh;
+
+import java.util.Scanner;
+
+public class StudentRun {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			while(true) {
+				System.out.println("이름을 입력하세요 : ");
+				String inputName = sc.nextLine();
+				System.out.println("아이디를 입력하세요 : ");
+				String inputId = sc.nextLine();
+				
+				Student ej = new Student();
+				ej.setName(inputName);
+				ej.setId(inputId);
+				System.out.println(ej.getName());
+				System.out.println(ej.getId());
+				//ej.name = inputName;
+				//ej.exam(ej.name);
+				
+				System.out.println("이름을 입력하세요 : ");
+				inputName = sc.nextLine();
+				System.out.println("아이디를 입력하세요 : ");
+				inputId = sc.nextLine();
+				
+				Student jw = new Student();
+				jw.setName(inputName);
+				jw.setId(inputId);
+				
+				String inputStr = sc.nextLine(); // String은 ==을 사용하면 안됨
+				if(inputStr.equals("퇴근")) {     // Stirng 비교는 equals()사용
+					break; 
+				}
+			}
+		}
+	}
+}

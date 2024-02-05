ac633474f1fff889f10c6882c5fee1f499540116
user1 <user1@DESKTOP-KJ3O3NO>
2024-02-05 11:16:05 +0900

    array

--- Homework/src/inclass/kh/TestArray.java
+++ Homework/src/inclass/kh/TestArray.java
@@ -4,9 +4,14 @@ public class TestArray {
 
 	public static void main(String[] args) {	
 		//배열 Array
-		String[] arrSeason = {"봄", "여름","가을","겨울"};
+		String[] arrSeason = {"봄", "여름","가을","겨울"};   //0부터 시작
 		
+		System.out.println(arrSeason[0]);   //봄
 		System.out.println(arrSeason[1]);   //여름
+		System.out.println(arrSeason[3]);   //가을
+		System.out.println(arrSeason[4]);   //겨울
+		
+		arrSeason[1] = "초여름";
 	}
 
 }

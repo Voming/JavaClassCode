c0bdcea4cdb76b206cf60af59d6495a2a53ce678
Voming <ebomin1234@gmail.com>
2024-02-06 10:05:55 +0900

    실습예제

--- Homework/src/review/kh/ExamArray.java
+++ Homework/src/review/kh/ExamArray.java
@@ -2,9 +2,14 @@ package review.kh;
 
 public class ExamArray {
 	public static void main(String[] args) {
-		int arr1[] = new int[] {1,2,3,4,5};
-		for(int i =0; i<arr1.length; i++) {
-			System.out.println(arr1[i]);
+		int arr1[] = new int[] { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < arr1.length; i++) {
+			if (i < arr1.length - 1) {
+				System.out.print(arr1[i] + ", ");
+			} else {
+				System.out.print(arr1[i]);
+			}
 		}
+		System.out.println();
 	}
 }

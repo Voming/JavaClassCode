86c3eeb0670070f05e40580f9f3c1205b68b5006
Voming <ebomin1234@gmail.com>
2024-02-06 14:40:29 +0900

    static test

--- JavaCode/src/inclass/kh/BankAccount.java
+++ JavaCode/src/inclass/kh/BankAccount.java
@@ -1,43 +1,56 @@
 package inclass.kh;
 
 public class BankAccount {
-	private static int amount;
-	
-	private String no;
+	private/* public 쓰는경우도 있음 */ static int amount = 5; // 클래스 변수
+
+	private String no; // 멤버 변수
 	private String password;
 	private String owner;
 	private String cid;
 	private int balance;
-	
-	/*public BankAccount() {    //기본 생성자 생략 가능
-		
-	}*/
-	
-	public BankAccount(String owner, String cid,String password) { //제한이 있는 생성자
+
+	/*
+	 * public BankAccount() { //기본 생성자 생략 가능
+	 * }
+	 */
+
+	public BankAccount(String owner, String cid, String password) { // 제한이 있는 생성자
 		this.owner = owner;
 		this.cid = cid;
 		this.password = password;
-	}
-	
+		if (BankAccount.amount == 0) {
+			System.out.println("더이상 계좌 개설이 불가능합니다");
+			return;
+		}
+		BankAccount.amount--; // 접근할 때 this 말고 클래스 명을 사용 권장
+		this.no = "";
+	}  //제힌이 있는 생성자
+
 	public void deposite(int input) {
 		balance += input;
 	}
-	
+
 	public void redraw(int input) {
-		if(balance < input) {
+		if (balance < input) {
 			System.out.println("잔액부족");
-			return;   // 먼저 빠저나가게 하고
+			return; // 먼저 빠저나가게 하고
 		}
-		balance -= input;   //정상적인 식 실행
+		balance -= input; // 정상적인 식 실행
 	}
-	
-	public int getAmount() {
-		return amount;
+
+	//많이 쓰이고 자주 호출되는 메소드에 static을 붙임
+	public static int getAmount() {
+		return BankAccount.amount;
 	}
-	
-	public void setAmount(int amount) {
-		this.amount = amount;
+
+	public static void setAmount(int amount) { // 생성이 되는 시점에만 설정하게 함부로 접근하지 못하게 함
+		BankAccount.amount = amount;
 	}
+
+	/*
+	 * private void setAmount(int amount) { //생성이 되는 시점에만 설정하게 함부로 접근하지 못하게 함
+	 * BankAccount.amount = amount; }
+	 */
 
 	public String getNo() {
 		return no;
@@ -62,6 +75,5 @@ public class BankAccount {
 	public int getBalance() {
 		return balance;
 	}
-	
-	
+
 }
--- JavaCode/src/inclass/kh/BankRun.java
+++ JavaCode/src/inclass/kh/BankRun.java
@@ -1,34 +1,44 @@
 package inclass.kh;
 
 public class BankRun {
+	public static void method1() {
+		System.out.println("그냥 static 클래스 메소드");
+	}
+
 	public static void main(String[] args) {
-		String no;
-		String password;
-		String owner;
-		String cid;
-		int balance;
+		// 여기 main 첫머리에서 static의 값 확인 가능
+		System.out.println(BankAccount.getAmount()); // 함수에도 static이 있어야 사용가능 
+		//많이 쓰이고 자주 호출되는 메소드에 static을 붙임
 
 		System.out.println("고객님 반갑습니다.");
 		System.out.println("성함과 주민번호를 주시면 계좌를 개설해 드릴게요.");
-		
-		BankAccount yi = new BankAccount("yi","11111","2323");
-		BankAccount jw = new BankAccount("jw","22222","1234");
-		BankAccount cw = new BankAccount("cw","33333","5555");
-		
+
+		// 같은 패키지 내부라 앞에 import를 하지 않아도 된다.
+		BankAccount yi = new BankAccount("yi", "11111", "2323"); // 인스턴스 : 자료형이 참조형, new로 생성해서 씀
+		BankAccount jw = new BankAccount("jw", "22222", "1234"); // 본인만의 heap공간이 있음
+		BankAccount cw = new BankAccount("cw", "33333", "5555");
+		/*
+		 * BankAccount af = new BankAccount("cw","33333","5555"); 
+		 * BankAccount sd = new BankAccount("cw","33333","5555"); 
+		 * BankAccount qe = new BankAccount("cw","33333","5555");
+		 */
+
 		jw.deposite(1000);
 		yi.deposite(50000);
-		
+
 		System.out.println(jw.getBalance());
 		System.out.println(cw.getBalance());
-		
-		jw.setAmount(1);    //== BankAccount.setAmount(1);
-		System.out.println(jw.getAmount());   //모두 1이나옴
-		System.out.println(cw.getAmount());   //1
-		System.out.println(yi.getAmount());   //1
-		
-		yi.setAmount(100);
-		System.out.println(jw.getAmount());   //모두 100이나옴
-		System.out.println(cw.getAmount());   //100
-		System.out.println(yi.getAmount());   //100
+
+		BankAccount.setAmount(1); // 실습 jw.setAmount(1);
+		System.out.println(BankAccount.getAmount());
+		/*System.out.println(jw.getAmount()); // 모두 1이나옴
+		System.out.println(cw.getAmount()); // 1
+		System.out.println(yi.getAmount()); // 1*/
+
+		BankAccount.setAmount(100);
+		System.out.println(BankAccount.getAmount());
+		/*System.out.println(jw.getAmount()); // 모두 100이나옴
+		System.out.println(cw.getAmount()); // 100
+		System.out.println(yi.getAmount()); // 100*/
 	}
 }

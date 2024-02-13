7abb4b7ea3bc46c49b5d1cd4bd2d0816578fbc78
Voming <ebomin1234@gmail.com>
2024-02-13 15:40:11 +0900

    문자열 버블

--- JavaClassCode/src/inclass/kh/week3/BubbleTest.java
+++ JavaClassCode/src/inclass/kh/week3/BubbleTest.java
@@ -2,13 +2,14 @@ package inclass.kh.week3;
 
 public class BubbleTest {
 	public static void main(String[] args) {
-		Double[] arr = new Double[] { 3.1, 2.5, 5.1, 8.3, 1.0, 4.5, 6.7 };
+		//Double[] arr = new Double[] { 3.1, 2.5, 5.1, 8.3, 1.0, 4.5, 6.7 };
+		String[] arr = new String[] {"다","마","가","라","파","마","나","바"};
 
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - i -1; j++) {
-				if (arr[j] > arr[j + 1]) // 오름차순 //if(arr[i] < arr[i+1]) 내림차순
+				if (arr[j].compareTo(arr[j + 1]) > 0) // /if(arr[i] > arr[i+1]) 오름차순 //if(arr[i] < arr[i+1]) 내림차순
 				{
-					Double temp = arr[j];
+					String temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 				}
@@ -25,4 +26,9 @@ public class BubbleTest {
 		}
 		System.out.println();
 	}
+	
+	//compareTo()반환값
+	//0: 두개의 문자열이 동일
+	//양수 : 호출하는 객체가 인자보다 사전적으로 순서가 앞설 때(크기가 클때) -> a > b
+	//음수 : 인자가 객체보다 사전적으로 순서가 앞설 때 -> a < b
 }

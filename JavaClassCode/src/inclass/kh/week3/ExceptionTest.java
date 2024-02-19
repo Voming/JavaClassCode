7bcc795d74821eb32c7f4e5d7add8713d166300c
Voming <ebomin1234@gmail.com>
2024-02-19 10:36:12 +0900

    InnerClass

--- JavaClassCode/src/inclass/kh/week3/ExceptionTest.java
+++ JavaClassCode/src/inclass/kh/week3/ExceptionTest.java
@@ -4,21 +4,56 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.List;
+import java.io.IOException;
 
 public class ExceptionTest {
-	public static void main(String[] args) {
-		// Unhandled exception type FileNotFoundException
+	public static void main(String[] args) throws FileNotFoundException {
+		method1();
+	}
+
+	public static void method1() throws FileNotFoundException {
+		// try catch 대신 throws로 처리함
+		// 해당 메소드를 쓰는 모든 곳에서 똑같이 throws를 선언해줘야함
+		BufferedReader br = new BufferedReader(new FileReader(new File("")));
+		System.out.println("파일이 없습니다.");
+	}
+
+	public void method() {
+		BufferedReader br = null;
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(new File("")));
+			br = new BufferedReader(new FileReader("C:/data/text.txt"));
+			String s;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s);
+			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.println("파일이 없습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null) // null일수도 있어서 null아닐때만 하도록
+					br.close(); // close실행할때도 예외가 발생 할 수 있기때문에 try catch로 묶어줌
+			} catch (IOException e) {
+			}
+		}
+
+	}
+
+	public void name() {
+		try (BufferedReader br = new BufferedReader(new FileReader("C:/data/text.txt"))) {
+			String s;
+			while ((s = br.readLine()) != null) {
+				System.out.println(s);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 없습니다.");
+		} catch (IOException e) {
 			e.printStackTrace();
 
-		} finally {
-			System.out.println("실행 완료");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	
 }

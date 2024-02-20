962798509aa20dbda45abf5a7d297ed50c8eb019
Voming <ebomin1234@gmail.com>
2024-02-20 14:11:06 +0900

    TCP 실습완료

--- JavaClassCode/src/inclass/kh/week4/tcp/TCPEchoClient.java
+++ JavaClassCode/src/inclass/kh/week4/tcp/TCPEchoClient.java
@@ -1,24 +1,66 @@
 package inclass.kh.week4.tcp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class TCPEchoClient {
 	public void tcpClient(String ip, int port) {
+		Scanner sc = new Scanner(System.in);
 		Socket cs = null;
+		BufferedReader br = null;
+		
+		PrintWriter pw = null;
+		
 		try {
 			System.out.println("sever에 접속 요청.....");
+			//3. 서버에 접속하는 소켓 
+			//4. 서버에서 리턴 된거 대입함 (양쪽이 동시성을 가지고 생성됨)
 			cs = new Socket(ip, port);
 			System.out.println("sever에 접속 완료!!");
+			
+//			서버에서 클라이언트로 받아옴
+//			br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+//			String msg = null;
+//			while ((msg = br.readLine()) != null) {
+//				System.out.println("Sever >" + msg);
+//			}
+			
+			// . // 입출력 스트림 얻어옴
+			pw = new PrintWriter(new OutputStreamWriter(cs.getOutputStream()));
+			br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+			
+			while(true){
+				System.out.print("전달 메시지 : ");
+				String imputMsg = sc.nextLine();
+
+				// 클라이언트에서 서버로 뿌림
+				pw.println(imputMsg);
+				pw.flush();
+			
+				//서버에서 클라이언트로 받아옴
+				String msg = null;
+				msg = br.readLine();
+				
+				if(msg == null) break;
+				System.out.println("echo >" + msg);
+			}
+			System.out.println("echo 끝");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if (cs != null)
-					cs.close();
+			try { // 생성의 반대 순서로 close() 해야함
+				if (pw != null) pw.close();
+				if (br != null) br.close();
+				if (cs != null) cs.close();
+				if (sc != null) sc.close();   //sc는 단독생성 되어서 close순서 상관 없음
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
--- JavaClassCode/src/inclass/kh/week4/tcp/TCPEchoSever.java
+++ JavaClassCode/src/inclass/kh/week4/tcp/TCPEchoSever.java
@@ -1,24 +1,62 @@
 package inclass.kh.week4.tcp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPEchoSever {
-	public void tcpSever(/*byte[] ip, */ int port) {
-		//port 지정하지 않는 다면 windodw가 남는 port 자동할당
+	public void tcpSever(/* byte[] ip, */ int port) {
+		// port 지정하지 않는 다면 windodw가 남는 port 자동할당
 		ServerSocket ss = null;
+		Socket connectedClients = null;
+		PrintWriter pw = null;
+
+		BufferedReader br = null;
 		try {
-			ss = new ServerSocket(port); //port넣으면 자동X
+			//1. 서버 소켓 생성
+			ss = new ServerSocket(port); // port넣으면 자동X
 			System.out.println(ss.getInetAddress().getHostAddress() + " ====client 접속 대기");
-			Socket connectedClients = ss.accept(); //client 접속을 기다림, scanner의 next와 유사함(계속 대기함)
+			
+			//2. 3. client 접속을 기다림 , scanner의 next와 유사함(계속 대기함) 
+			//4. 통신이 가능한 소켓이 만들어짐
+			connectedClients = ss.accept();
 			System.out.println(connectedClients.getInetAddress().getHostAddress() + " ====client 접속 완료");
 			
+			
+//			OutputStream os = connectedClients.getOutputStream();  혼선을 막기위해 아래 처럼 넣어줌
+//			OutputStreamWriter osw = new OutputStreamWriter(connectedClients.getOutputStream());
+//			osw.write("hello client");  //OutputStreamWriter 줄바꿈 같은 기호가 잘 안됨
+//			osw.flush();
+//
+//			서버에서 클라이언트로 뿌림
+//			pw = new PrintWriter(new OutputStreamWriter(connectedClients.getOutputStream()));
+//			pw.println("HELLO CLIENT"); // PrintWriter를 사용하면 메소드가 훨씬 풍성해짐
+//			pw.flush();
+			
+			// . // 입출력 스트림 얻어옴
+			br = new BufferedReader(new InputStreamReader(connectedClients.getInputStream()));
+			pw = new PrintWriter(new OutputStreamWriter(connectedClients.getOutputStream()));
+			String msg = null;
+			// 클라이언트에서 서버로 받아옴
+			while ((msg = br.readLine()) != null) {
+				System.out.println("Client >" + msg);
+				
+				//읽고나서 바로 클라이언트로 Write하지 않으면 연결 끊김
+				pw.println("~~" + msg);
+				pw.flush();	
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if(ss != null) ss.close();
+			try { // 생성의 반대 순서로 close() 해야함
+				if (pw != null) pw.close();  //pw와 br의 close 순서는 상관없음
+				if (br != null) br.close();
+				if (connectedClients != null) connectedClients.close();
+				if (ss != null) ss.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
